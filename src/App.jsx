b4d2ada243d7c2c0e5e3a89af4bc77529a42660c
dive0 <zhengshiwei5@gmail.com>
2023-04-07 14:00:50 -0400

    Add content and style in home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { useState, useEffect } from "react";
 import NavBar from "./Components/NavBar";
 import { supabase } from "./client";
 import { Routes, Route, Outlet } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Create from "./pages/Create";
 import Gallery from "./pages/Gallery";
+import Details from "./pages/Details";
+import Edit from "./pages/Edit";
 
 function App() {
   const [crew, setCrew] = useState([]);
@@ -17,21 +20,32 @@ function App() {
         .order("created_at", { ascending: true });
 
       setCrew(data);
-    }
+    };
     fetchData().catch(console.error);
-
-  }, [])
+  }, []);
 
   return (
     <div className="w-screen h-screen bg-gray-900">
       <NavBar />
       <Routes>
-        <Route path="/" element={<Home />}  />
+        <Route path="/" element={<Home />} />
         <Route path="/create" element={<Create />} />
         <Route path="/gallery" element={<Gallery />} />
+        <Route path="/:id">
+          <Route index element={<Details />} />
+          <Route path="edit" element={<Edit />} />
+        </Route>
+        <Route
+          path="*"
+          element={
+            <p className="text-white text-center text-4xl mt-10">
+              404 Page Not Found
+            </p>
+          }
+        />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import Crewmates from "./../assets/crewmates.43d07b24.png";
+import Ship from "./../assets/spaceship.3d8f767c.png";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="text-white text-center">
+      <h1 className="text-7xl my-6">Welcome to the Crewmate Creator!</h1>
+      <p className="text-3xl my-3">Here is where you can create your very own set of crewmates before sending them off into space!</p>
+
+      <div className="flex flex-col items-center">
+        <img src={Crewmates} alt="crewmates" width="600" height="400" />
+        <img src={Ship} alt="Ship" width="600" height="400"/>
+      </div>
+    </div>
   )
 }
 
