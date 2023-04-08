2c242b93b59751a9b16382e32289a1b8921f6e7c
dive0 <zhengshiwei5@gmail.com>
2023-04-08 13:36:00 -0400

    Add and style the crewmate detail page

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,13 +5,13 @@ const Card = (props) => {
   const { crewmate } = props;
 
   return (
-    <div className="bg-gray-500 px-7 py-4 m-7 text-2xl">
+    <div className="bg-gray-500 px-7 py-4 m-7 text-2xl rounded-2xl">
       <Link to={`/${crewmate.id}`}>
         <div className="space-y-3">
           <img src={outline} alt="outline" width="200" className="mx-auto" />
-          <p>Name of Crewmate: {crewmate.Name}</p>
-          <p>Speed of Crewmate: {crewmate.Speed}</p>
-          <p>Color of Crewmate: {crewmate.Color}</p>
+          <p>Name of Crewmate: <span>{crewmate.Name}</span></p>
+          <p>Speed of Crewmate: <span>{crewmate.Speed} mph</span></p>
+          <p>Color of Crewmate: <span>{crewmate.Color}</span></p>
         </div>
       </Link>
 
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Details = () => {
+import suspect from "./../assets/suspect.bdfacc7e.png"
+
+const Details = (props) => {
+  const { crew } = props;
+
+  const { id } = useParams();
+  const mate = crew.filter((item) => item.id == id)[0];
+
   return (
-    <div>Details</div>
-  )
-}
+    <div className="page">
+      <h1>Crewmate: {mate.Name}</h1>
+      <h2 className="text-5xl my-3">Stats:</h2>
+      <div className="text-3xl">
+        <p className="my-3">Color: {mate.Color}</p>
+        <p>Speed: {mate.Speed} mph</p>
+        {mate.Speed > 70 ? (
+          <p className="mt-20">
+            Wow, this Crewmate is super fast, that will be helpful! 🏃💨
+          </p>
+        ) : (
+          <p className="mt-20">
+            You may want to find a Crewmate with more speed, this one is kind of
+            slow 😬
+          </p>
+        )}
+      </div>
+      <Link to={`/${mate.id}/edit`}>
+        <button>Want to edit this crewmate?</button>
+      </Link>
 
-export default Details
+      <img src={suspect} alt="suspect" width="200"/>
+    </div>
+  );
+};
+
+export default Details;
